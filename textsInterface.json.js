db7e03c821cf54d4b5ab99907d71a74e962d2b01
cmohge1 <christopherohge@gmail.com>
2021-06-14 13:42:57 +0100

    restoring original code

--- textsInterface.json.js
+++ textsInterface.json.js
@@ -2,120 +2,164 @@ var texts =
     {
         "headline":
             {
-                "english": "The Book Historian's Digital Tool-kit (LRBS 2021)"
+                "english": "Catalogue of the Rudolf Geyer Collection",
+                "german": "Katalog von dem Nachlass Rudolf Geyer"
+            }
+        ,
+        "stiftFlorianTip":
+            {
+                "english": "Go to Stift Florian's Website",
+                "german": "Gehe zur Website von Stift Florian"
+            }
+        ,
+        "digitalOrientalistTip":
+            {
+                "english": "Made by L.W.C. van Lit",
+                "german": "hergestellt von Dr. L.W.C. van Lit"
+            }
+        ,
+        "flagTip":
+            {
+                "english": "Switch to English",
+                "german": "Auf deutsch umstellen"
             }
         ,
         "welcomeHeader":
             {
-                "english": "Welcome"
+                "english": "Welcome",
+                "german": "Herzlich willkommen"
             }
         ,
         "welcomeBody":
             {
-                "english": "Welcome to the catalogue of the Rudolf Geyer Collection at Stift Florian. This collection spans more than 1.500 titles, collected by Rudolf Geyer († 1929), professor at Vienna University and scholar of Arabic literature. The collection consists mostly of printed books, from both Europe and the Middle East, all of them from the 19th century or early 20th century. A handful of manuscripts are also preserved. Use the search box below to narrow your focus, and click on the circle with an i next to the title for more information. The sort functions are fairly limited, only sorting alphabetical or numerical low to high. If no value is given, it is sorted to the top. For Author sorting, preference is given to first author, then editor, then translator."
+                "english": "Welcome to the catalogue of the Rudolf Geyer Collection at Stift Florian. This collection spans more than 1.500 titles, collected by Rudolf Geyer († 1929), professor at Vienna University and scholar of Arabic literature. The collection consists mostly of printed books, from both Europe and the Middle East, all of them from the 19th century or early 20th century. A handful of manuscripts are also preserved. Use the search box below to narrow your focus, and click on the circle with an i next to the title for more information. The sort functions are fairly limited, only sorting alphabetical or numerical low to high. If no value is given, it is sorted to the top. For Author sorting, preference is given to first author, then editor, then translator.",
+                "german": "Willkommen beim Katalog dem Nachlass Rudolf Geyer im Stift Florian. Diese Kollektion umfasst mehr als 1.500 Titel, gesammelt von Rudolf Geyer († 1929), Professor an der Universität Wien und Gelehrter für arabische Literatur. Die Sammlung besteht hauptsächlich aus gedruckten Büchern aus Europa und dem Nahen Osten, alle aus dem 19. oder frühen 20. Jahrhundert. Eine Handvoll Manuskripte sind ebenfalls erhalten. Verwenden Sie das Suchfeld, um den Fokus einzugrenzen, und klicken Sie auf den Kreis mit einem i neben dem Titel, um weitere Informationen zu erhalten. Die Sortierfunktionen sortieren nur alphabetisch oder numerisch von niedrich nach hoch. Wenn kein Wert angegeben ist, wird dieser nach oben sortiert. Bei der Autorensortierung wird der Verfasser, dann der Herausgeber und dann der Übersetzer bevorzugt."
             }
         ,
         "searchButton":
             {
-                "english": "Search"
+                "english": "Search",
+                "german": "suchen"
             }
         ,
         "searchBox":
             {
-                "english": "Search for..."
+                "english": "Search for...",
+                "german": "suchen nach..."
             }
         ,
         "searchTip":
             {
-                "english": "Type a term and press Enter or click Search"
+                "english": "Type a term and press Enter or click Search",
+                "german": "Geb einen Begriff ein und drück auf die Eingabetaste oder klick auf Suchen"
             }
         ,
         "sortBy":
             {
-                "english": "Sort by: "
+                "english": "Sort by: ",
+                "german": "Sortiere nach: "
             }
         ,
         "sortTitleCaption":
             {
-                "english": "Title"
+                "english": "Title",
+                "german": "Titel"
             }
         ,
         "sortAuthorCaption":
             {
-                "english": "Author"
+                "english": "Author",
+                "german": "Verfasser"
             }
         ,
         "sortYearCaption":
             {
-                "english": "Year"
+                "english": "Year",
+                "german": "Jahre"
             }
         ,
         "beginTitleCatalog":
             {
-                "english": "All entries of the Catalogue"
+                "english": "All entries of the Geyer Catalogue",
+                "german": "Alle Einträge des Geyer-Katalogs"
             }
         ,
         "noResultsTitleCatalog":
             {
-                "english": "No results, showing all entries instead:"
+                "english": "No results, showing all entries instead:",
+                "german": "Keine Ergebnisse, stattdessen werden alle Einträge angezeigt:"
             }
         ,
         "againTitleCatalog":
             {
-                "english": "Showing all entries again:"
+                "english": "Showing all entries again:",
+                "german": "Alle Einträge erneut angezeigt:"
             }
         ,
         "foundItemsCatalog":
             {
-                "english": "Number of results:"
+                "english": "Number of results:",
+                "german": " Anzahl der Ergebnisse:"
             },
         "authorCatalog":
             {
-                "english": "Author: "
+                "english": "Author: ",
+                "german": "Verfasser: "
             }
         ,
         "editorCatalog":
             {
-                "english": "Editor: "
+                "english": "Editor: ",
+                "german": "Herausgeber: "
             },
         "translatorCatalog":
             {
-                "english": "Translator: "
+                "english": "Translator: ",
+                "german": "Übersetzer: "
             },
         "noAuthorCatalog":
             {
-                "english": "No author details. "
+                "english": "No author details. ",
+                "german": "Keine Autorendetails. "
             },
         "numberVolumesCatalog":
             {
-                "english": "Number of volumes: "
+                "english": "Number of volumes: ",
+                "german": "Anzahl Banden: "
             },
         "publisherCatalog":
             {
-                "english": "Publisher: "
+                "english": "Publisher: ",
+                "german": "Verlag: "
             },
         "placeCatalog":
             {
-                "english": "Place: "
+                "english": "Place: ",
+                "german": "Stelle: "
             },
         "yearCatalog":
             {
-                "english": "Year: "
+                "english": "Year: ",
+                "german": "Jahr: "
             },
         "additionalCatalog":
             {
-                "english": "Additional information on title page: "
+                "english": "Additional information on title page: ",
+                "german": "Weitere Informationen auf der Titelseite: "
             },
         "callNumberCatalog":
             {
-                "english": "Call Number: "
+                "english": "Call Number: ",
+                "german": "Rufnummer: "
             },
         "urlCatalog":
             {
-                "english": "Click here for a digitized copy"
+                "english": "Click here for a digitized copy",
+                "german": "Klicken Sie hier für eine digitalisierte Kopie"
             },
         "closeCatalog":
             {
-                "english": "Close"
+                "english": "Close",
+                "german": "schließen"
             }
     }
